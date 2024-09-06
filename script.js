5d0f306caa41c53c9f9d5bbe2fce1c51fdc5e64e
Julian Gnz <63946576+GonzDv@users.noreply.github.com>
2024-09-06 16:18:17 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const starsContainer = document.querySelector(".stars");
+
+// Create 100 star elements
+for (let i = 0; i < 100; i++) {
+  const star = document.createElement("div");
+  star.classList.add("star");
+  star.style.left = `${Math.random() * 100}%`;
+  star.style.top = `${Math.random() * 100}%`;
+  star.style.animationDelay = `${Math.random() * 5}s`;
+  starsContainer.appendChild(star);
+}
+// typewriter effect
+var p = document.getElementById("typewriter");
+var text = "<Student Web Developer />"; /* The text */
+
+function textTypingEffect(element, text, i = 0) {
+  element.textContent += text[i];
+
+  if (i === text.length - 1) {
+    return;
+  }
+  setTimeout(() => textTypingEffect(p, text, i + 1), 50);
+}
+textTypingEffect(p, text);
